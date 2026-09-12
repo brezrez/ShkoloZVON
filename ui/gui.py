b55b7f0371dbe641ff036638acebe7c4017e894c
Uroka09 <zalex9002@gmail.com>
2026-09-12 15:22:36 +0300

    aleks huesos

---
+++ ui/gui.py
@@ -0,0 +1,23 @@
+import flet as ft
+from flet.controls import alignment
+
+
+def main(page: ft.Page):
+    page.title = 'ШколоZVOн'
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    start_button = ft.Button('Start')
+
+
+
+
+    page.add(
+
+    )
+
+
+
+
+
+ft.app(target=main)
