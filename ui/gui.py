29ae9587fe7ee09d9449e4dd1fd2d56da1415a1c
Uroka09 <zalex9002@gmail.com>
2026-09-13 12:57:34 +0300

    append spiskov

--- ui/gui.py
+++ ui/gui.py
@@ -6,8 +6,11 @@ import flet as ft
 def main(page: ft.Page):
     page.title = 'ШколоZVOн'
 
+    timetable = ft.TextField(label='Укажите не укажите')
+
+
     def get_clicked(e):
-        bell_list.controls = [ft.TextField(label='Укажите не укажите'), ft.Button('Добавить звонок', on_click=new_bell)]
+        bell_list.controls = [timetable, ft.Button('Добавить звонок', on_click=new_bell)]
         dialog_window.open = True
         page.update()
 
@@ -25,23 +28,54 @@ def main(page: ft.Page):
                 i.content.controls[0].value = not i.content.controls[0].value
         page.update()
         pass
+
+
+    schedule_zvonkov = ['Полное расписание', 'Сокращенное расписание', 'Залупа', '', '312312']
+
+
+    def new_timetable(e):
+        schedule_zvonkov.append(timetable.value)
+        page.close(dialog_window)
+        schedule.append(ft.Container(
+            ft.Row(
+                controls=[ft.Checkbox(value=False),
+                          ft.Text(spans=[ft.TextSpan(timetable.value, on_click=lambda e: click_schedule(e), data=timetable.value)], size=20),
+                          dialog_window]
+            ),
+            width=400,
+            alignment=ft.alignment.Alignment(0, 0),
+            border_radius=24,
+            padding=20,
+            shadow=ft.BoxShadow(
+                spread_radius=1,
+                blur_radius=15,
+                color=ft.Colors.BLUE_GREY_300,
+                offset=ft.Offset(0, 0),
+                blur_style=ft.ShadowBlurStyle.OUTER),
+            bgcolor="white",
+            data=timetable.value
+        ))
+        view_schedule.update()
+        page.update()
+        print(schedule_zvonkov)
+
+
     global bell_list
-    bell_list = ft.Column([ft.TextField(label='Укажите не укажите'), ft.Button('Добавить звонок', on_click=new_bell)], spacing=15, width=450, height=450, scroll=ft.ScrollMode.AUTO)
+    bell_list = ft.Column([ft.TextField(label='Название расписания'), ft.Button('Добавить звонок', on_click=new_bell)], spacing=15, width=450, height=450, scroll=ft.ScrollMode.AUTO)
 
     dialog_window = ft.AlertDialog(
         title=ft.Text("Редактировать задание"),
         content=bell_list,
         actions=[
             ft.Button("Отмена",on_click = lambda e: page.close(dialog_window)),
-            ft.ElevatedButton("Сохранить"),
+            ft.Button("Сохранить", on_click=new_timetable),
         ],
     )
 
 
 
-        
 
-    schedule_zvonkov = ["1 расписание", "2 расписание", "3 расписание"]
+
     schedule = []
 
     for i in schedule_zvonkov:
@@ -49,7 +83,8 @@ def main(page: ft.Page):
         schedule.append(ft.Container(
             ft.Row(
                 controls=[ft.Checkbox(value=False),
-                          ft.Text(spans=[ft.TextSpan(i, on_click=lambda e: click_schedule(e), data=i)],size=20),dialog_window]
+                          ft.Text(spans=[ft.TextSpan(i, on_click=lambda e: click_schedule(e), data=i)], size=20),
+                          dialog_window]
             ),
             width=400,
             alignment=ft.alignment.Alignment(0, 0),
@@ -64,6 +99,7 @@ def main(page: ft.Page):
             bgcolor="white",
             data=i
         ))
+
     view_schedule = ft.Row(
         [
             ft.Column(
